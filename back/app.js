f17b5ef56e48938974ace48c9ae9aa6a3be5a4fe
Monia-L <monia.laoufi@gmail.com>
2019-08-04 17:39:02 +0200

    clean code

--- back/app.js
+++ back/app.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require('body-parser')
-const cors = require('cors')
 // Getting all routes
 const routes = require("./routes/get_data")
 
@@ -13,7 +12,6 @@ const app = express();
 
 // Middlewares
 app.use(morgan("dev"));
-app.use(cors());
 
 // Body Parser configuration
 app.use(bodyParser.json({limit: '10mb', extended: true}))
